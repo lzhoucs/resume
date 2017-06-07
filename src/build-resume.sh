ac46029a6dc87576abcd42d060c311c4efe984a5
Liang Zhou <lzhoucs@gmail.com>
2017-06-07 00:11:05 -0500

    Clean up out dir based on condition. Refactoring.

--- src/build-resume.sh
+++ src/build-resume.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
 cd "$(dirname "$0")"
+build_dir="../out"
 
 # pdf version
-rm -r ../out
+if [ -d "$build_dir" ]; then
+    rm -r $build_dir
+fi
 hackmyresume build resume.json to ../out/resume.all -t awesome
 cd ../out/latex/examples
 xelatex resume.tex
@@ -11,5 +14,5 @@ mv resume.pdf ../../../resume.pdf
 cd ../../../src
 
 # html version
-rm -r ../out
+# rm -r $build_dir
 hackmyresume build resume.json to ../index.html
