146ec61bf6cd6849da604b3c630a87a5f12bb22d
Liang Zhou <lzhoucs@gmail.com>
2017-06-06 22:40:03 -0500

    Make build script portable so it can be executed from any dir. Add html version into build script.

--- src/build-resume.sh
+++ src/build-resume.sh
@@ -1,5 +1,15 @@
+#!/bin/bash
+
+cd "$(dirname "$0")"
+
+# pdf version
 rm -r ../out
 hackmyresume build resume.json to ../out/resume.all -t awesome
 cd ../out/latex/examples
 xelatex resume.tex
 mv resume.pdf ../../../resume.pdf
+cd ../../../src
+
+# html version
+rm -r ../out
+hackmyresume build resume.json to ../index.html
